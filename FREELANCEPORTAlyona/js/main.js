a68a0fad6df578e0d2663d51d39cd09f5b73123b
olena-star <122024787+olena-star@users.noreply.github.com>
2024-01-02 13:05:51 +0100

    darktheme

---
+++ FREELANCEPORTAlyona/js/main.js
@@ -0,0 +1,17 @@
+const btnDarkMode = document.querySelector(".dark-mode-btn");
+
+if(localStorage.getItem('darkMode') ==='dark'){
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add('dark')
+}
+
+btnDarkMode.onclick= function(){
+    
+    btnDarkMode.classList.toggle("dark-mode-btn--active");
+const isDark = document.body.classList.toggle('dark');
+if(isDark){
+    localStorage.setItem('darkMode','dark')
+}else{
+    localStorage.setItem('darkMode','light');
+}
+}
